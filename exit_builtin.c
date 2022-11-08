8e5948734901ac12d3ff04d722bef032afc5ee30
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2022-11-08 09:32:54 +0100

    Fix exit builtin error behavior

--- exit_builtin.c
+++ exit_builtin.c
@@ -13,40 +13,6 @@
 #include "builtins.h"
 #include "environment_utils.h"
 
-static int	is_valid_arg(const char *arg)
-{
-	int	i;
-
-	if (arg == NULL)
-		return (0);
-	i = 0;
-	while (str_chr(" \t\r\n\v\f", arg[i]) != NULL)
-		++i;
-	if (arg[i] == '+' || arg[i] == '-')
-		++i;
-	while (arg[i] != '\0')
-	{
-		if (arg[i] < '0' || arg[i] > '9')
-			return (0);
-		++i;
-	}
-	return (1);
-}
-
-static int	has_valid_arguments(t_command *command)
-{
-	size_t	i;
-
-	i = 1;
-	while (i < command->nb_args)
-	{
-		if (!is_valid_arg(command->args[i]))
-			return (0);
-		++i;
-	}
-	return (1);
-}
-
 static int	str_status_to_int(const char *str, int *has_error)
 {
 	long long	val;
@@ -76,24 +42,36 @@ static int	str_status_to_int(const char *str, int *has_error)
 	return ((int)val);
 }
 
+static int	is_valid_arg(const char *arg)
+{
+	int	has_error;
+
+	str_status_to_int(arg, &has_error);
+	return (has_error == 0);
+}
+
+static int	has_valid_arguments(t_command *command)
+{
+	size_t	i;
+
+	i = 1;
+	while (i < command->nb_args)
+	{
+		if (!is_valid_arg(command->args[i]))
+			return (0);
+		++i;
+	}
+	return (1);
+}
+
 static int	get_exit_status(t_command *command)
 {
 	int	status;
 	int	has_error;
 
 	if (command->nb_args > 2 && has_valid_arguments(command))
-	{
-		write(2, "minishell: exit: too many arguments\n", 36);
 		return (1);
-	}
 	status = str_status_to_int(command->args[1], &has_error);
-	if (!is_valid_arg(command->args[1]) || has_error)
-	{
-		write(2, "minishell: exit: ", 17);
-		write(2, command->args[1], str_len(command->args[1]));
-		write(2, ": numeric argument required\n", 28);
-		return (255);
-	}
 	return (status);
 }
 
@@ -105,14 +83,22 @@ int	exit_builtin(t_command *command, t_command *commands)
 		exit_status = 0;
 	else
 		exit_status = get_exit_status(command);
-	if (nb_commands(commands) > 1)
-		return (exit_status);
-	write(command->output.fd, "exit\n", 5);
+	if (nb_commands(commands) == 1)
+		write(command->output.fd, "exit\n", 5);
+	if (command->nb_args > 2 && is_valid_arg(command->args[1]))
+		write(2, "minishell: exit: too many arguments\n", 36);
+	else if (command->nb_args >= 2 && !is_valid_arg(command->args[1]))
+	{
+		write(2, "minishell: exit: ", 17);
+		write(2, command->args[1], str_len(command->args[1]));
+		write(2, ": numeric argument required\n", 28);
+		exit_status = 255;
+	}
 	if (command->nb_args <= 2 || !is_valid_arg(command->args[1]))
 	{
 		free_commands(commands);
 		clear_env_list();
 		exit(exit_status);
 	}
-	return (1);
+	return (exit_status);
 }
